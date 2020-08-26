d50ab497d11a6df738344fcbe6b68165df829314
MrSayyod <ssayyod@gmail.com>
2020-08-27 03:33:57 +0500

    Add validation to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
   def destroy
     @user = User.find(params[:id])
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/,
+  message: "should contain only letters" }
 end
