26cfccdf0402f8da7e320bf378ab27f3a52c2f3b
MrSayyod <ssayyod@gmail.com>
2020-08-27 00:44:26 +0500

    Create 'update' method in 'Users' controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to @user, notice: "Account successfully updated!"
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
 end
