6e908c232f52f3e4e01c186fb97d9469f8812700
MrSayyod <ssayyod@gmail.com>
2020-08-29 03:11:54 +0500

    Create appropriate changes to templates and methods for signing out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user
     else
       render :new
