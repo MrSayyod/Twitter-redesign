32d7f17ca3d9c9416601f9d0af035f1e13f419e7
MrSayyod <ssayyod@gmail.com>
2020-09-03 20:07:54 +0500

    Set validations and associations to 'Post' model, and run migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/,
   message: "should contain only letters" }
+
+  has_many :posts, dependent: :destroy
 end
