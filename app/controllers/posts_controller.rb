601717d4ad8656d93a7d3ab8791948b1ea4657d3
MrSayyod <ssayyod@gmail.com>
2020-09-10 16:58:47 +0500

    Add editing and updating posts options by user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,34 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all.order("created_at DESC")
+    @post = Post.new
   end
 
   def edit
     @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @user = User.find(params[:id])
+    @post.update(post_params)
+    redirect_to @user, notice: "Post successfully updated!"
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    @user = User.find(params[:id])
+    redirect_to @user
+  end
+
+  def new
+    
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
