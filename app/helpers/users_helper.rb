1d8c9f1d573c5bafc385a5b7e353d0def9ee90fa
MrSayyod <ssayyod@gmail.com>
2020-10-08 00:53:47 +0500

    Create helper method for user avatar, paste avatar on user page

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def avatar_for(user, options = { size: 80 })
+  size = options[:size]
+    if user.avatar?
+      image_tag user.avatar.url(:thumb), width: size
+    else
+      image_tag 'default-avatar.png', width: size
+    end
+  end
 end
