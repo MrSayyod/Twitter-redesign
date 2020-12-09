c16c7196b1ba33bf87f458460759e86b401add5c
MrSayyod <ssayyod@gmail.com>
2020-12-09 20:39:05 +0500

    Write test for validations and associations of Followship model

---
+++ spec/models/followship_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Followship, type: :model do
+  
+  describe "associations" do
+    it { should belong_to(:follower).class_name('User') }
+    it { should belong_to(:followed).class_name('User') }
+  end
+  
+  describe "validations" do
+    it { should validate_presence_of(:follower_id) }
+    it { should validate_presence_of(:followed_id) }
+  end
+end
