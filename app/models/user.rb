0d1b4ca3dfb077a0fb4f952be562f68060dfc81d
MrSayyod <ssayyod@gmail.com>
2020-12-09 19:18:20 +0500

    Write tests for validations and associations of User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: {minimum: 2}, format: { with: /\A[a-zA-Z]+\z/,
-  message: "should contain only letters" }
+  message: "should contain only letters" }, uniqueness: true
 
   has_many :posts, dependent: :destroy
   has_many :active_relationships, class_name: "Followship", foreign_key: "follower_id", dependent: :destroy
