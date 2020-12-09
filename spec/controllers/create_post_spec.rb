61e867caac8266a04404e7863832312f0b83e9de
MrSayyod <ssayyod@gmail.com>
2020-12-09 23:07:11 +0500

    Write tests for creating posts, following and unfollowing

---
+++ spec/controllers/create_post_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'Create post', type: :feature do
+  describe 'Creating post by user' do
+    before(:each) do
+      User.create(username: 'User')
+      visit 'session/new'
+      fill_in 'username', with: 'User'
+      click_button 'Log In'
+    end
+
+    it 'should create a new post' do
+      fill_in 'post_body', with: "Hello World!"
+      click_button 'Post'
+      expect(page).to have_content('Hello World!')
+    end
+
+  end
+end
