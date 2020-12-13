9cde16688dccbf038ea28face7b7a7e69f887b21
MrSayyod <ssayyod@gmail.com>
2020-12-13 16:21:12 +0500

    Fix errors after moving logic from controller to model

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,6 +42,22 @@ class PostsController < ApplicationController
 
   private
 
+  def timeline_posts
+    @posts = Post.user_posts(current_user) + Post.user_posts(current_user.following)
+    @posts
+  end
+
+  # def timeline_posts
+  #   @user_posts = Post.all.where(user: current_user)
+  #   @followers_posts = Post.all.where(user: current_user.following)
+  #   @post_list = @user_posts + @followers_posts
+  #   @post_list
+  # end
+
+  # def timeline_posts
+  #   self.timeline_posts
+  # end
+
   def post_params
     params.require(:post).permit(:body)
   end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,10 +4,22 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { in: 1..280 }
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
-  def timeline_posts
-    @user_posts = Post.all.where(user: current_user)
-    @followers_posts = Post.all.where(user: current_user.following)
-    @post_list = @user_posts + @followers_posts
-    @post_list
+  def self.user_posts(user)
+    self.all.where(user: user)
   end
+
+  # def followers_posts
+  #   self.all.where(user: current_user.following)
+  # end
+
+  # def timeline_posts
+  #   user_posts + followers_posts
+  # end
+
+    # def timeline_posts
+  #   @user_posts = self.all.where(user: current_user)
+  #   @followers_posts = self.all.where(user: current_user.following)
+  #   @post_list = @user_posts + @followers_posts
+  #   @post_list
+  # end
 end
