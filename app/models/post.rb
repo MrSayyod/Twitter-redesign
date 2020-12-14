fcb977fc030b45e19a4c1ae30371a70b49eed397
MrSayyod <ssayyod@gmail.com>
2020-12-14 23:55:19 +0500

    Delete empty spaces from index page, add tests for page usage, such as error messages when try to create empty content, and fix rubocop errors

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,21 +5,6 @@ class Post < ApplicationRecord
   scope :ordered_by_most_recent, -> { order(created_at: :desc) }
 
   def self.user_posts(user)
-    self.all.where(user: user)
+    all.where(user: user)
   end
-
-  # def followers_posts
-  #   self.all.where(user: current_user.following)
-  # end
-
-  # def timeline_posts
-  #   user_posts + followers_posts
-  # end
-
-    # def timeline_posts
-  #   @user_posts = self.all.where(user: current_user)
-  #   @followers_posts = self.all.where(user: current_user.following)
-  #   @post_list = @user_posts + @followers_posts
-  #   @post_list
-  # end
 end
--- spec/controllers/create_post_spec.rb
+++ spec/controllers/create_post_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature 'Create post', type: :feature do
+  let(:user) do
+    User.create(
+      username: 'User'
+    )
+  end
+  let(:post) do
+    Post.new(body: 'This is a post', user_id: user.id)
+  end
   describe 'Creating post by user' do
     before(:each) do
       User.create(username: 'User')
@@ -13,6 +21,27 @@ RSpec.feature 'Create post', type: :feature do
       fill_in 'post_body', with: 'Hello World!'
       click_button 'Post'
       expect(page).to have_content('Hello World!')
+    end
+
+    it 'shows an error when try to post empty content' do
+      fill_in 'post_body', with: ''
+      click_button 'Post'
+      expect(page).to have_content('Body can\'t be blank')
+    end
+  end
+end
+
+RSpec.feature 'Create post', type: :feature do
+  describe 'validations' do
+    it 'should not be valid without a content' do
+      post.body = nil
+      expect(post).to_not be_valid
+    end
+
+    it 'should have a content less than 280 characters' do
+      post2 = Post.create(body: 'a' * 1001, user_id: user.id)
+      expect(post).to be_valid
+      expect(post2).to_not be_valid
     end
   end
 end
