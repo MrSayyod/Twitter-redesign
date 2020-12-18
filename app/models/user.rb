0684080b8d32d8866f57e1ee4eff37e6651da789
MrSayyod <ssayyod@gmail.com>
2020-12-18 19:05:29 +0500

    Add maximal length for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, length: { minimum: 2 }, format: { with: /\A[a-zA-Z]+\z/,
+  validates :username, presence: true, length: { in: 2..30 }, format: { with: /\A[a-zA-Z]+\z/,
                                                                          message: 'only letters allowed' }
   validates :username, uniqueness: true
 
